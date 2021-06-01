1eb5d232cf5f7aac043ddb12544f03eb6378341e
ravitmg <tmgrawv@gmail.com>
2021-06-01 18:00:31 +0545

    clean up

--- cypress/integration/Volunteering.spec.js
+++ cypress/integration/Volunteering.spec.js
@@ -1,41 +1,66 @@
-describe("Create a new Volunteering Activity", () => {
-    it("Add Volunteering Activity", () => {
-      const link = "Contacts"; 
-      const link2 = "Volunteering";
+describe('Create a new Volunteering Activity', () => {
+	it('Add Volunteering Activity', () => {
+		const link = 'Contacts'
+		const link2 = 'Volunteering'
 
-      cy.visit("https://dev.keela.co");
-        cy.fixture("user").then((user) => {
-             const { email, password ,firstName, lastName, activityName, description} = user;
+		cy.visit('https://dev.keela.co')
+		cy.fixture('user').then(user => {
+			const {
+				email,
+				password,
+				firstName,
+				lastName,
+				activityName,
+				description,
+			} = user
 
-          cy.login(email, password);
-          cy.clickSidebar(link);
+			cy.login(email, password)
+			cy.clickSidebar(link)
 
-          cy.get("#main-container").get(".content-scroll-header").get(".btn").contains("Add Contact").click();
-              cy.get('[data-tracking-input="contacts-PersonContacts-firstName"]').within(() => {
-               cy.get("input").type(firstName)
-            }); 
-              cy.get('[data-tracking-input="contacts-PersonContacts-lastName"]').within(() => {
-                cy.get("input").type(lastName)
-              });
-                 cy.get(".btn").contains("Save").click();
-                   cy.pageHeader(link2);
+			cy.get('#main-container')
+				.get('.content-scroll-header')
+				.get('.btn')
+				.contains('Add Contact')
+				.click()
+			cy.get(
+				'[data-tracking-input="contacts-PersonContacts-firstName"]'
+			).within(() => {
+				cy.get('input').type(firstName)
+			})
+			cy.get('[data-tracking-input="contacts-PersonContacts-lastName"]').within(
+				() => {
+					cy.get('input').type(lastName)
+				}
+			)
+			cy.get('.btn').contains('Save').click()
+			cy.pageHeader(link2)
 
-            
-                cy.get("#main-container").get(".block").get(".btn").contains("Add Activity").click();
-                cy.get('[data-tracking-input="contact-volunteering-VolunteerRecords-name"]').within(() => {
-                cy.get("input").type(activityName)
-                });
+			cy.get('#main-container')
+				.get('.block')
+				.get('.btn')
+				.contains('Add Activity')
+				.click()
+			cy.get(
+				'[data-tracking-input="contact-volunteering-VolunteerRecords-name"]'
+			).within(() => {
+				cy.get('input').type(activityName)
+			})
 
-                cy.get('[data-tracking-input="contact-volunteering-VolunteerRecords-description"]').within(() => {
-                  cy.get("textarea").type(description)
-                });  
+			cy.get(
+				'[data-tracking-input="contact-volunteering-VolunteerRecords-description"]'
+			).within(() => {
+				cy.get('textarea').type(description)
+			})
 
-                cy.get("#modals-container").get(".block-content").get(".btn").contains("Save").click();
-              
-                    cy.get(".vue-notification-group").should("contain", " Volunteering Activity Successfully added ")
-                  //  cy.get('[data-tracking-table="contact-volunteering-VolunteerRecords-0-0"]').within(()=>{
-                  //   cy.get("td").get(".font-w500").should("contain", activityName)
-                  });
-                });
-              });
-              
+			cy.get('#modals-container')
+				.get('.block-content')
+				.get('.btn')
+				.contains('Save')
+				.click()
+
+			cy.get('.notification-title', { timeout: 10000 }).contains(' Volunteering Activity Successfully added', { matchCase: false })
+			//  cy.get('[data-tracking-table="contact-volunteering-VolunteerRecords-0-0"]').within(()=>{
+			//   cy.get("td").get(".font-w500").should("contain", activityName)
+		})
+	})
+})
